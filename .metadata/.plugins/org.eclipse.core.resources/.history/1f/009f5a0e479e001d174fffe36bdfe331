97aa3c29b7178b40b6e629301e1fa3e929a9d7c3
ysnslk <yasin.solak.26@gmail.com>
2023-01-27 16:42:18 +0300

    2. değişiklikler yapıldı

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/1f/009f5a0e479e001d174fffe36bdfe331
@@ -0,0 +1,10 @@
+package lesson001;
+
+public class Deneme {
+
+	public static void main(String[] args) {
+		System.out.println("Deneme");
+
+	}
+
+}
--- BilgeAdamAlgoritma/src/lesson001/Deneme.java
+++ BilgeAdamAlgoritma/src/lesson001/Deneme.java
@@ -4,7 +4,12 @@ public class Deneme {
 
 	public static void main(String[] args) {
 		System.out.println("Deneme");
+		// Ctrl +f11 çalıştırma
 
+		// update için
+		// git add .
+		// git commit -m "2. değişiklikler yapıldı"
+		// git push
 	}
 
 }
