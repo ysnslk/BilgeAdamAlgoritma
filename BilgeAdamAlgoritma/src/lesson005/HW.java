c64b77dac12f2eb73d87ecd79363755392f1a1f1
ysnslk <yasin.solak.26@gmail.com>
2023-02-03 17:01:58 +0300

    Arrays

---
+++ BilgeAdamAlgoritma/src/lesson005/HW.java
@@ -0,0 +1,25 @@
+package lesson005;
+
+public class HW {
+
+	public static void main(String[] args) {
+		//Question 43 için 
+		//1
+		
+		
+		
+		//2
+		// Bir dizide en büyük ikinci sayıyı bulan program.
+		
+		
+		
+		//3
+		//Bir dizide eğer 13 var ise hem 13 ü  hem de bir sonraki indexi 
+		//Toplama işlemine almayacak 
+		//Dizideki elemanların toplamını bulalım
+		
+		
+
+	}
+
+}
