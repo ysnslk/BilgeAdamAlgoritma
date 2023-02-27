6abc49e22fb4b3fd66612d76e492672a94539880
ysnslk <yasin.solak.26@gmail.com>
2023-02-27 16:58:44 +0300

    Exams

---
+++ BilgeAdamAlgoritma/src/lesson015/Main.java
@@ -0,0 +1,29 @@
+package lesson015;
+
+public class Main {
+	
+	/*
+	 * Mülakatta sorulur Abstract sınıflar ve interface farkı
+	 * 
+	 * Interfaceler aslında bir sınıfın nasıl davracanacağını söyler Interfacelerde
+	 * gövdeli metod yazabilir miyiz?
+	 * 
+	 * interfacelerde java 8 ile birlikte static keywordu sayesinde gövdeli method
+	 * yazabiliyoruz . interfacelerde java 8 ile birlikte default keywordu
+	 * sayesinde gövdeli method yazabiliyoruz
+	 * 
+	 * 
+	 * Polymorphism ?
+	 * 
+	 * Bir nesnenin farklı nesnelerinin referansını tutabilme özelliğidir.
+	 * 
+	 * 
+	 */
+
+	public static void main(String[] args) {
+
+		
+
+	}
+
+}
---
+++ BilgeAdamAlgoritma/src/lesson015/veteriner/Insan.java
@@ -0,0 +1,36 @@
+package lesson015.veteriner;
+
+public abstract class Insan {
+
+	private String tcKimlik;
+	private String isim;
+
+	public Insan(String tcKimlik, String isim) {
+		super();
+		this.tcKimlik = tcKimlik;
+		this.isim = isim;
+	}
+
+	public String getTcKimlik() {
+		return tcKimlik;
+	}
+
+	public void setTcKimlik(String tcKimlik) {
+		this.tcKimlik = tcKimlik;
+	}
+
+	public String getIsim() {
+		return isim;
+	}
+
+	public void setIsim(String isim) {
+		this.isim = isim;
+	}
+	public abstract void bilgileriGoster();
+
+	@Override
+	public String toString() {
+		return "Insan [tcKimlik=" + tcKimlik + ", isim=" + isim + "]";
+	}
+
+}
