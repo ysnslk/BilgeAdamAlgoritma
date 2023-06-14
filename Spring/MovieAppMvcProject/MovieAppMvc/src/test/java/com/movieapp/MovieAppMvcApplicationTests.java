4adef75afee7759b1dc1199a1e3185ace47fe914
ysnslk <yasin.solak.26@gmail.com>
2023-06-14 14:06:52 +0300

    Login And Movies Details

---
+++ Spring/MovieAppMvcProject/MovieAppMvc/src/test/java/com/movieapp/MovieAppMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movieapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieAppMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
