0cc7ba8bfebb3efefa7a4c7557f68d9fea091204
ysnslk <yasin.solak.26@gmail.com>
2023-07-10 13:52:00 +0300

    -

---
+++ Spring/Yemek-Sepeti-Demo-Uygulama/src/test/java/com/yemeksepeti/demo/YemekSepetiDemoUygulamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yemeksepeti.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YemekSepetiDemoUygulamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
